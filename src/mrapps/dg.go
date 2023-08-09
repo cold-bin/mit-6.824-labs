750f5f7eb861418f4df85ca11eb2bc5e6049ef0a
刘海斌 <3398341353@qq.com>
2023-08-09 21:22:41 +0800

    feat: 添加challenge1
    分布式grep

---
+++ src/mrapps/dg.go
@@ -0,0 +1,40 @@
+// @author cold bin
+// @date 2023/8/9
+
+package main
+
+import (
+	"6.5840/mr"
+	"fmt"
+	"log"
+	"strings"
+)
+
+func Map(filename string, contents string) []mr.KeyValue {
+	// 找出含有`good`单词的每一行
+	builder := strings.Builder{}
+	ans := make([]mr.KeyValue, 0, 1000)
+	line := 0
+	for _, r := range []rune(contents) {
+		if r == '\n' || r == '\r' {
+			line++
+			if strings.Contains(contents, "good") {
+				ans = append(ans, mr.KeyValue{Key: fmt.Sprint(line), Value: builder.String()})
+			}
+			builder.Reset()
+		} else {
+			builder.WriteRune(r)
+		}
+	}
+
+	return ans
+}
+
+func Reduce(key string, values []string) string {
+	if len(values) != 1 {
+		log.Println("len is not one")
+		return ""
+	}
+
+	return values[0]
+}
