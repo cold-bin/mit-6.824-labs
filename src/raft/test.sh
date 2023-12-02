b3f60011f788b58ccbcd01d161853aeaac8da183
刘海斌 <3398341353@qq.com>
2023-12-02 10:02:51 +0800

    test: 新增raft lab脚本测试文件

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,4 +1,37 @@
-#!/usr/bin/zsh
-for i in {1..100} ; do
-  VERBOSE=1 go test -race -run 2B >> test_times_100
-done
+#!/usr/bin/env bash
+
+cnt=$2
+if [[ !($cnt =~ ^[0-9]+$) ]]; then
+    echo "$cnt 不是数字"
+    echo "usage: test.sh which_item test_times"
+    exit
+fi
+
+function tests() {
+    num=$1
+    while [ $num -gt 0 ]; do
+        VERBOSE=1 go test -race -run 2$2 >> test2A_$3_times
+        let "num--"
+    done
+}
+
+num=$cnt
+
+case $1 in
+  A)
+    echo "Test lab2A ${cnt} times..." >> test2A_${cnt}_times
+    tests $cnt 'A' $cnt
+    ;;
+  B)
+    echo "Test lab2B ${cnt} times..." >> test2B_${cnt}_times
+    tests $cnt 'B' $cnt
+    ;;
+  C)
+    echo "Test lab2C ${cnt} times..." >> test2C_${cnt}_times
+    tests $cnt 'C' $cnt
+    ;;
+  *)
+    echo "invalid $1"
+    echo "usage: test.sh which_item test_times"
+    ;;
+esac
